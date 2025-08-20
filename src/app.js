168f70f583e6b3f5c7e2e9ce16f4c14b1103cf8e
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-08-19 21:59:25 -0300

    atualização no app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors';    
 import Routes from './app/routes/Routes.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors({
     // origin: 'http://localhost:5173', // seu frontend React
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
 }))
 
